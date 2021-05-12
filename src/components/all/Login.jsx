0eaa208efc9899736be3d2b9d2b0b7c164fecaad
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-12 14:56:03 +0700

    update login page interface

--- src/components/all/Login.jsx
+++ src/components/all/Login.jsx
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
+import "../../App.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div>
-      <p>Welcome</p>
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
+    <div className="wrap">
+      <div className="content-login-page">
+        <div className="putih">
+          <div className="header">
+            <h1>Welcome back!</h1>
+            <h1>Glad to have you back</h1>
+          </div>
+          <form action="">
+            <div className="input-wrap">
+              <div class="mb-3">
+                <input type="email" className="form-control" id="exampleInputPassword1" placeholder="Email" />
+              </div>
+              <div class="mb-3">
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+              </div>
+            </div>
+            <div className="btn-wrap">
+              <a href="" className="login-btn">
+                <button type="button" class="btn btn-primary">
+                  Login
+                </button>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
