09a1c8f551c2b3888a09fa17d58ef2dcec60f18c
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-17 10:29:10 +0700

    add admin page and update routers

--- src/Routers.js
+++ src/Routers.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/all/Login";
+
 import HomeActive from "./components/teknisi/homepageactive";
 import HomeDone from "./components/teknisi/homepagedone";
 import HomeProgress from "./components/teknisi/homepageprogress";
 import Profile from "./components/teknisi/profile";
 import EditProfile from "./components/teknisi/editprofile";
 
+import ProjectActive from "./components/admin/projectpageactive";
+import ProjectDone from "./components/admin/projectpagedone";
+import ProjectProgress from "./components/admin/projectpageprogress";
+import Bid from "./components/admin/bidpage";
+import CreateProject from "./components/admin/createproject";
+import EditProject from "./components/admin/editproject";
+import UserPage from "./components/admin/userpage";
+import CreateUser from "./components/admin/createuser";
+import EditUser from "./components/admin/edituser";
+
 const Routers = () => {
   return (
     <Router>
       <Switch>
+        {/* Start All */}
         <Route exact path="/">
           <Login />
         </Route>
+        {/* End All */}
+
+        {/* Start Teknisi */}
         <Route path="/active">
           <HomeActive />
         </Route>
@@ -29,6 +44,37 @@ const Routers = () => {
         <Route path="/profile">
           <Profile />
         </Route>
+        {/* End Teknisi */}
+
+        {/* Start Admin */}
+        <Route path="/admin/project-active">
+          <ProjectActive />
+        </Route>
+        <Route path="/admin/project-done">
+          <ProjectDone />
+        </Route>
+        <Route path="/admin/project-progress">
+          <ProjectProgress />
+        </Route>
+        <Route path="/admin/project-active/bid">
+          <Bid />
+        </Route>
+        <Route path="/admin/project-active/create-project">
+          <CreateProject />
+        </Route>
+        <Route path="/admin/project-active/edit-project">
+          <EditProject />
+        </Route>
+        <Route path="/admin/user-page">
+          <UserPage />
+        </Route>
+        <Route path="/admin/user-page/create-user">
+          <CreateUser />
+        </Route>
+        <Route path="/admin/user-page/edit-user">
+          <EditUser />
+        </Route>
+        {/* End Admin */}
       </Switch>
     </Router>
   );
