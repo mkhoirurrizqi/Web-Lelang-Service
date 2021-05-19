97eb4f00a4aef388772d9e6a94b5b80a728c8912
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-19 23:41:17 +0700

    connect userpage to api

--- src/components/admin/userpage.jsx
+++ src/components/admin/userpage.jsx
@@ -1,7 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavbarA from "../admin/navbaradmin";
+import { useSelector } from "react-redux";
 
 const UserPage = () => {
+  const role = useSelector((data) => data.user.role);
+  const token = useSelector((data) => data.user.token);
+  const id = useSelector((data) => data.user.id);
+
+  const [userArray, setUserArray] = useState([]);
+
+  useEffect(() => {
+    fetch("https://web-lelang.herokuapp.com/api/showalluser", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then((responseJson) => {
+        console.log(responseJson);
+        setUserArray([]);
+        responseJson.forEach((element) => {
+          setUserArray((userArray) => [
+            ...userArray,
+            {
+              id: element.id,
+              name: element.name,
+              nim: element.nim,
+              nohp: element.nohp,
+              role: element.role,
+              username: element.username,
+              email: element.email,
+            },
+          ]);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
       <NavbarA />
@@ -15,7 +57,7 @@ const UserPage = () => {
           <table className="table table-light table-striped table-hover table-bordered">
             <thead>
               <tr>
-                <th scope="col">id</th>
+                <th scope="col">Numb</th>
                 <th scope="col">Role</th>
                 <th scope="col">Name</th>
                 <th scope="col">NIM</th>
@@ -26,44 +68,29 @@ const UserPage = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th>1</th>
-                <td>Admin</td>
-                <td>Bambang Pamungkas</td>
-                <td>118140831</td>
-                <td>bambang.118140831@student.itera.ac.id</td>
-                <td>bepe831</td>
-                <td>085260007000</td>
-                <td>
-                  <div className="btn-group" role="group" aria-label="Basic outlined button group">
-                    <a type="button" className="btn btn-primary" href="/user/edit">
-                      Edit
-                    </a>
-                    <form action="" method="post" className="d-inline">
-                      <button className="btn btn-warning">Delete</button>
-                    </form>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <th>2</th>
-                <td>Technician</td>
-                <td>Bruno Mars</td>
-                <td>118140901</td>
-                <td>bruno.118140901@student.itera.ac.id</td>
-                <td>brunooo</td>
-                <td>081912344321</td>
-                <td>
-                  <div className="btn-group" role="group" aria-label="Basic outlined button group">
-                    <a type="button" className="btn btn-primary" href="/user/edit">
-                      Edit
-                    </a>
-                    <form action="" method="post" className="d-inline" onclick="return confirm('Yakin mengarsipkan data?')">
-                      <button className="btn btn-warning">Delete</button>
-                    </form>
-                  </div>
-                </td>
-              </tr>
+              {userArray.map((user, i) => {
+                return (
+                  <tr key={i}>
+                    <th>{i + 1}</th>
+                    <td>{user.role}</td>
+                    <td>{user.name}</td>
+                    <td>{user.nim}</td>
+                    <td>{user.email}</td>
+                    <td>{user.username}</td>
+                    <td>{user.nohp}</td>
+                    <td>
+                      <div className="btn-group" role="group" aria-label="Basic outlined button group">
+                        <a type="button" className="btn btn-primary" href="/user/edit">
+                          Edit
+                        </a>
+                        <form action="" method="post" className="d-inline">
+                          <button className="btn btn-warning">Delete</button>
+                        </form>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
