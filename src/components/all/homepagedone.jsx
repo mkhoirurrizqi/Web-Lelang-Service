866b549d4c8978dbb485ffa932dc5b4c2f8de2f8
alifahmaaad <ahmad.118140133@student.itera.ac.id>
2021-05-19 23:04:21 +0700

    connect homepagedone to api

--- src/components/all/homepagedone.jsx
+++ src/components/all/homepagedone.jsx
@@ -6,86 +6,92 @@ import {ReactNumberFormat} from "./numberformat";
 
 const HomeDone = (props) => {
   const role = useSelector(data => data.user.role);
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const [initprice, setInitPrice] = useState("");
-  const [fixprice, setFixPrice] = useState("");
-  const [location, setLocation] = useState("");
-  const [hardware, setHardware] = useState("");
-  const [technician, setTechnician] = useState("");
+  const [storeArray, setStoreArray] = useState([]);
   const token = useSelector((data) => data.user.token);
-
+  useEffect(() => {
+    fetch("https://web-lelang.herokuapp.com/api/doneproject", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then((responseJson) => {
+        setStoreArray([]);
+        responseJson.forEach((element) => {
+          setStoreArray((storeArray) => [
+            ...storeArray,
+            {
+              judul: element.judul,
+              harga_awal: element.harga_awal,
+              harga_fix: element.harga_fix,
+              deskripsi: element.deskripsi,
+              lokasi: element.lokasi,
+              jenis: element.jenis,
+              tanggal_akhir_bid: element.tanggal_akhir_bid,
+              user_id: element.user_id,
+            },
+          ]);
+        });
+        console.log(responseJson);
+        console.log(storeArray);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }, []);
+    const getusername = (id) => {
+      if(id){
+        fetch("https://web-lelang.herokuapp.com/api/getusername", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          }, body: JSON.stringify({
+            user_id: id,
+          }),
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then((responseJson) => {
+            return responseJson.name;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }else{
+        return "-";
+      }
+          };
   return (
     <div>
       {role != "admin" ? <NavbarT /> : <NavbarA />}
       <div className="container">
         <div className="content-done-project">
           <div className="row">
-          <div className="card">
+          {storeArray.map((store, i) => {
+            return (
+              <div className="card">
               <div className="card-body">
-                <h3 className="text-center card-title project-title">Laptop Blackscreen</h3>
-                <p className="card-text project-desc">Ketika laptop dihidupkan, tidak tampila apa-apa, hanya layar hitam</p>
+                <h3 className="text-center card-title project-title">{store.judul}</h3>
+                <p className="card-text project-desc">{store.deskripsi}</p>
               <div className="project-field">
-                <p className="text-center project-initial-price">Open Price : <ReactNumberFormat value={initprice} /></p>
-                <p className="text-center project-fixed-price">Fixed Price : <ReactNumberFormat value={fixprice} /></p>
-                <p className="text-center project-location">Location : Lampung Selatan</p>
-                <p className="text-center project-hardware-type">Type : Laptop</p>
-                <p className="text-center project-technisian">Technisian : Rizqi</p>
+                <p className="text-center project-initial-price">Open Price : <ReactNumberFormat value={store.harga_awal} /></p>
+                <p className="text-center project-fixed-price">Fixed Price : <ReactNumberFormat value={store.harga_fix} /></p>
+                <p className="text-center project-location">Location : {store.lokasi}</p>
+                <p className="text-center project-hardware-type">Type : {store.jenis}</p>
+                <p className="text-center project-technisian">Technisian : {getusername(store.user_id)}</p>
                 </div>
               </div>
             </div>
-            <div className="card">
-              <div className="card-body">
-                <h3 className="text-center card-title project-title">Laptop Blackscreen</h3>
-                <p className="card-text project-desc">Ketika laptop dihidupkan, tidak tampila apa-apa, hanya layar hitam</p>
-              <div className="project-field">
-                <p className="text-center project-initial-price">Open Price : Rp. 35000</p>
-                <p className="text-center project-fixed-price">Fixed Price : Rp. 40000</p>
-                <p className="text-center project-location">Location : Lampung Selatan</p>
-                <p className="text-center project-hardware-type">Type : Laptop</p>
-                <p className="text-center project-technisian">Technisian : Rizqi</p>
-                </div>
-              </div>
-            </div> 
-            <div className="card">
-              <div className="card-body">
-                <h3 className="text-center card-title project-title">Laptop Blackscreen</h3>
-                <p className="card-text project-desc">Ketika laptop dihidupkan, tidak tampila apa-apa, hanya layar hitam</p>
-              <div className="project-field">
-                <p className="text-center project-initial-price">Open Price : Rp. 35000</p>
-                <p className="text-center project-fixed-price">Fixed Price : Rp. 40000</p>
-                <p className="text-center project-location">Location : Lampung Selatan</p>
-                <p className="text-center project-hardware-type">Type : Laptop</p>
-                <p className="text-center project-technisian">Technisian : Rizqi</p>
-                </div>
-              </div>
-            </div> 
-            <div className="card">
-              <div className="card-body">
-                <h3 className="text-center card-title project-title">Laptop Blackscreen</h3>
-                <p className="card-text project-desc">Ketika laptop dihidupkan, tidak tampila apa-apa, hanya layar hitam</p>
-              <div className="project-field">
-                <p className="text-center project-initial-price">Open Price : Rp. 35000</p>
-                <p className="text-center project-fixed-price">Fixed Price : Rp. 40000</p>
-                <p className="text-center project-location">Location : Lampung Selatan</p>
-                <p className="text-center project-hardware-type">Type : Laptop</p>
-                <p className="text-center project-technisian">Technisian : Rizqi</p>
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-body">
-                <h3 className="text-center card-title project-title">Laptop Blackscreen</h3>
-                <p className="card-text project-desc">Ketika laptop dihidupkan, tidak tampila apa-apa, hanya layar hitam</p>
-              <div className="project-field">
-                <p className="text-center project-initial-price">Open Price : Rp. 35000</p>
-                <p className="text-center project-fixed-price">Fixed Price : Rp. 40000</p>
-                <p className="text-center project-location">Location : Lampung Selatan</p>
-                <p className="text-center project-hardware-type">Type : Laptop</p>
-                <p className="text-center project-technisian">Technisian : Rizqi</p>
-                </div>
-              </div>
-            </div>
+           );
+          })}
           </div>
         </div>
       </div>
