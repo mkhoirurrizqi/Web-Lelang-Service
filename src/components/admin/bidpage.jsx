db5bf76facd0da75326f1256cd08102c438faee4
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-21 18:26:52 +0700

    add confirm box bidder page

--- src/components/admin/bidpage.jsx
+++ src/components/admin/bidpage.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NavbarA from "../admin/navbaradmin";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -18,7 +18,6 @@ const BidPage = (props) => {
     history.push("/active");
   }
   useEffect(() => {
-    
     fetch("https://web-lelang.herokuapp.com/api/showbidproject", {
       method: "POST",
       headers: {
@@ -40,94 +39,93 @@ const BidPage = (props) => {
           setStoreArray((storeArray) => [
             ...storeArray,
             {
-        user_id:element.user_id,
-        bidder:element.name,
-        id:element.id,
-        created_at:element.created_at,
-        harga_tawar:element.harga_tawar,
-      },
-    ]);
-  });
+              user_id: element.user_id,
+              bidder: element.name,
+              id: element.id,
+              created_at: element.created_at,
+              harga_tawar: element.harga_tawar,
+            },
+          ]);
+        });
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
- 
-  const accbid = (idbid,harga_tawar) => {
+  const accbid = (idbid, harga_tawar) => {
     fetch("https://web-lelang.herokuapp.com/api/updateProjectbid", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: idproject,
-          user_id: idbid,
-          harga_fix:harga_tawar
-        }),
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: idproject,
+        user_id: idbid,
+        harga_fix: harga_tawar,
+      }),
+    })
+      .then(function (response) {
+        return response;
       })
-        .then(function (response) {
-          return response;
-        })
-        .then((responseJson) => {
-          console.log(responseJson);
-          delallbid(idbid);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      .then((responseJson) => {
+        console.log(responseJson);
+        delallbid(idbid);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   const delallbid = () => {
     fetch("https://web-lelang.herokuapp.com/api/delallbidproject", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          project_id: idproject
-        }),
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        project_id: idproject,
+      }),
+    })
+      .then(function (response) {
+        return response;
       })
-        .then(function (response) {
-          return response;
-        })
-        .then((responseJson) => {
-          console.log(responseJson);
+      .then((responseJson) => {
+        console.log(responseJson);
         history.go(0);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   const delbid = (idbid) => {
     console.log(idbid);
     console.log(token);
     fetch("https://web-lelang.herokuapp.com/api/delbidproject", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: idbid
-        }),
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: idbid,
+      }),
+    })
+      .then(function (response) {
+        console.log(response);
+        return response;
       })
-        .then(function (response) {
-          console.log(response);
-          return response;
-        })
-        .then((responseJson) => {
-          console.log(responseJson);
-          history.go(0);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      .then((responseJson) => {
+        console.log(responseJson);
+        history.go(0);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   return (
     <div>
@@ -140,39 +138,56 @@ const BidPage = (props) => {
             </button>
           </a>
           <div className="table-outter wrapper">
-          <table className="table table-light table-striped table-hover table-bordered ">
-            <thead>
-              <tr>
-                <th scope="col">id</th>
-                <th scope="col">Bidder</th>
-                <th scope="col">Asked Price</th>
-                <th scope="col">Bid On</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-            {storeArray.map((store, i) => {
-              return (
-              <tr key={i}>
-                <th scope="row">{store.id}</th>
-                <td>{store.bidder}</td>
-                <td>{store.harga_tawar}</td>
-                <td>{store.created_at}</td>
-                <td>
-                  <div className="btn-group" role="group" aria-label="Basic outlined button group">
-                    <button  onClick={() => accbid(store.user_id,store.harga_tawar)} className="btn btn-success" >
-                      Accept
-                    </button>
-                    
-                      <button onClick={() => delbid(store.id)} className="btn btn-danger">Reject</button>
-                    
-                  </div>
-                </td>
-              </tr>
-              );
-            })}
-            </tbody>
-          </table>
+            <table className="table table-light table-striped table-hover table-bordered ">
+              <thead>
+                <tr>
+                  <th scope="col">id</th>
+                  <th scope="col">Bidder</th>
+                  <th scope="col">Asked Price</th>
+                  <th scope="col">Bid On</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {storeArray.map((store, i) => {
+                  return (
+                    <tr key={i}>
+                      <th scope="row">{store.id}</th>
+                      <td>{store.bidder}</td>
+                      <td>{store.harga_tawar}</td>
+                      <td>{store.created_at}</td>
+                      <td>
+                        <div className="btn-group" role="group" aria-label="Basic outlined button group">
+                          <button
+                            onClick={() => {
+                              const confirmBox = window.confirm("Are you sure you want to accept this bid?");
+                              if (confirmBox == true) {
+                              }
+                              accbid(store.user_id, store.harga_tawar);
+                            }}
+                            className="btn btn-success"
+                          >
+                            Accept
+                          </button>
+
+                          <button
+                            onClick={() => {
+                              const confirmBox = window.confirm("Are you sure you want to reject this bid?");
+                              if (confirmBox == true) {
+                              }
+                              delbid(store.id);
+                            }}
+                            className="btn btn-danger"
+                          >
+                            Reject
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
